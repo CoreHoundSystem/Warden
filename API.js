5f2b5a21d2a077d57da9381e3e5232ab2dc22dec
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-07-25 17:13:57 -0400

    Update API.js

--- API.js
+++ API.js
@@ -39,8 +39,7 @@ function initClient() {
 			"userId": "me"
 		}).then(function(response) {
 		// Handle the results here (response.result has the parsed body).
-			console.log("Response", response);
-			console.log("Result", response.result);
+			console.log("Result", response.result.emailAddress);
 		})
 		
 	}, function(error) {
