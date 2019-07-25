546b0364e86a16a43aa9a87beefc1d4e24fc304a
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-07-25 16:18:58 -0400

    Update API.js

--- API.js
+++ API.js
@@ -23,7 +23,7 @@ function initClient() {
 		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 		// Handle the initial sign-in state.
 		updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-		onSignIn(gapi.auth2.currentUser.get());
+		onSignIn(gapi.auth2.getAuthInstance().isSignedIn.get());
 		authorizeButton.onclick = handleAuthClick;	//alter buttons as needed - cosmetic
 		signoutButton.onclick = handleSignoutClick;
 	}, function(error) {
