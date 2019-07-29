81e6ae22f46b20f9e17e98e0553edd4d6d59dea6
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-07-29 18:45:42 -0400

    Update accountSetup.js

--- accountSetup.js
+++ accountSetup.js
@@ -13,20 +13,30 @@ function getFileList(obj,respFunction) {
 }
 
 function searchAccount(response) {
+	parentIDs=response.result.emailAddress;
+	console.log(parentIDs);
 	if(response.result.files.length===0) {
 		console.log("Create new account in this drive.");
 	}
 	if(response.result.files.length===1) {
 		console.log("Account may exist, lets look some more...");
-		/*resourceData='';
-		gapi.client.drive.files.list({
-			q: "name = 'Users' and (mimeType = 'application/vnd.google-apps.folder')",
-			resource:resourceData;
-		}).then(function(response) {
-			
-		}*/
+		resourceData='parents':parentIDs[0];
+		obj={q: "name = 'Users' and (mimeType = 'application/vnd.google-apps.folder')",
+			resource:resourceData}
+		getFileList(obj,'confirmSolo');
 	}
 	if(response.result.files.length>1) {
 		console.log("Hmmm.... now what?");
+		isolateTrueWarden(parentIDs);
+	}
+}
+
+function confirmSolo(response) {
+	console.log("Only the lonely");
+}
+
+function isolateTrueWarden(pID) {
+	for(var i=0;i<pID.length;i++) {
+		console.log(pID[i]);
 	}
 }
