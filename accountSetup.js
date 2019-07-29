093036c8f7548143b5438173efa5a2209d98a0e2
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-07-29 18:27:49 -0400

    Update accountSetup.js

--- accountSetup.js
+++ accountSetup.js
@@ -1,10 +1,32 @@
 function newAccount(email) {
-	gapi.client.drive.files.list({
-		q: "name = 'Warden CRM' and (mimeType = 'application/vnd.google-apps.folder')"
-	}).then(function(response) {
-		console.log(response);
+	obj={q: "name = 'Warden CRM' and (mimeType = 'application/vnd.google-apps.folder')"};
+	getFileList(obj,'searchAccount');
+}
+
+function getFileList(obj,respFunction) {
+	gapi.client.drive.files.list(obj).then(function(response) {
+		window[respFunction(response)];
 	},
 	function(err) { 
 		console.error("Execute error",err);
 	});
+}
+
+function searchAccount(response) {
+	if(response.result.files.length===0) {
+		console.log("Create new account in this drive.");
+	}
+	if(response.result.files.length===1) {
+		console.log("Account may exist, lets look some more...");
+		resourceData='';
+		gapi.client.drive.files.list({
+			q: "name = 'Users' and (mimeType = 'application/vnd.google-apps.folder')"
+			resource:resourceData;
+		}).then(function(response) {
+			
+		}
+	}
+	if(response.result.files.length>1) {
+		console.log("Hmmm.... now what?");
+	}
 }
