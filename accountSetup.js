f88485ff2d348b06a14ea3069d52bbf11eff41be
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-07-29 17:22:46 -0400

    Update accountSetup.js

--- accountSetup.js
+++ accountSetup.js
@@ -1,8 +1,1 @@
-function newAccount(email) {
-	gapi.client.drive.files.list().then(function(response) {
-		console.log(response);
-	},
-	function(err) { 
-		console.error("Execute error",err);
-	});
-}
+
