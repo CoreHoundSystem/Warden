dfec4f56f252c28e5a6f9c971361126f92ec4ea2
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-07-29 17:01:17 -0400

    Update accountSetup.js

--- accountSetup.js
+++ accountSetup.js
@@ -1,1 +1,11 @@
-test
+function newAccount(email) {
+	gapi.client.drive.files.list({
+		"q": "name = 'Warden CRM',
+		(mimeType = 'application/vnd.google-apps.folder')"
+	}).then(function(response) {
+		console.log(response);
+	},
+	function(err) { 
+		console.error("Execute error",err);
+	});
+}
