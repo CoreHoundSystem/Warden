f363f3ad51851ffae899e96db5fa7fb898ef630e
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-07-30 06:23:25 -0400

    Update accountSetup.js

--- accountSetup.js
+++ accountSetup.js
@@ -83,8 +83,8 @@ function verifyAccountStructure() {
 }
 
 function createNewAccount() {
-	body={"majorDimension": "ROWS","range": "A1","values": [["My Test"]]};
-	obj={title: user.email,spreadsheetId:'1DdegvRj5fbrD8HyiLqWX5ZoVm0K7dgKPCUguiN6JweE',range:'A1',valueInputOption: ''};
+	body={"majorDimension": "ROWS","range": "A1","values": [["My Test"]],spreadsheetId:'1DdegvRj5fbrD8HyiLqWX5ZoVm0K7dgKPCUguiN6JweE'};
+	obj={title: user.email,range:'A1',valueInputOption: ''};
 	createSheet(obj,body,console.log,'test');
 }
 
