117f601e1a74bef53396da8d46c81c5142aa82e2
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-07-30 02:36:53 -0400

    Update accountSetup.js

--- accountSetup.js
+++ accountSetup.js
@@ -14,6 +14,7 @@ function getFileList(obj,respFunction,x) {
 	});
 }
 
+//determines if there are NO matches, a signle match, or multiple matches to the initial folder of the file structure.
 function searchAccount(response,x) {
 	console.log(response);
 	files=response.result.files;
@@ -46,7 +47,11 @@ function searchAccount(response,x) {
 	}
 	if(parentIDs.length>1) {
 		console.log("Hmmm.... now what?");
-		isolateTrueWarden(parentIDs);
+		//isolateTrueWarden(parentIDs);
+		
+		
+		obj={q: "name = 'Users' and (mimeType = 'application/vnd.google-apps.folder')",fields:'files(id,trashed,parents,owners(me,permissionId,emailAddress),ownedByMe)'};
+		getFileList(obj,'matchParents',parentIDs);
 	}
 }
 
@@ -68,10 +73,11 @@ function confirmSolo(response,x) {
 		}
 	}
 	console.log(user);
-	console.log(matchCounter);
+	//console.log(matchCounter);
 	console.log("Only the lonely");
+	//folder to move on to user's email address...
 }
-
+/*
 function isolateTrueWarden(parentIDs) {
 	trueParent='';
 	pIDs=[];
@@ -83,7 +89,7 @@ function isolateTrueWarden(parentIDs) {
 	obj={q: "name = 'Users' and (mimeType = 'application/vnd.google-apps.folder')",fields:'files(id,trashed,parents,owners(me,permissionId,emailAddress),ownedByMe)'};
 	getFileList(obj,'matchParents',parentIDs);
 }
-
+*/
 //this function takes an array 'Warden CRM' folder IDs and compares them to an array of 'User' folder IDs - should be made more versatile...
 function matchParents(response,x) {
 	console.log(response);		//response for a list of folders named 'User' - x represents an array of parent ID options
@@ -94,7 +100,6 @@ function matchParents(response,x) {
 		console.log(children[i].parents);
 		for(var j=0;j<x.length;j++) {
 			if(children[i].parents.indexOf(x[j])!=-1) {
-				console.log("MATCH!!");
 				masterFolderId.push(x[j]);
 				userFolderId.push(children[i].id);
 			}
@@ -107,6 +112,7 @@ function matchParents(response,x) {
 	if(masterFolderId.length==1) {
 		user.masterFolderId=masterFolderId[0];
 		user.usersFolderId=userFolderId[0];
+		console.log("Only the lonely");
 	}
 	if(masterFolderId.length>1) {
 		console.log("Hmmm.... now what?");
