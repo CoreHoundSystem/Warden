ace2632b9e4321f10e6c7cfe9a287277ddab707d
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-07-31 17:41:18 -0400

    Update API.js

--- API.js
+++ API.js
@@ -1,24 +1,52 @@
-function newAccount(email) {
-	window['USER_ID']=email;
-	window['user']={};
-	updateUser('email',email,'user');
-	console.log(user);
-	obj={spaces:'appDataFolder',fields:'files(id,trashed,parents,ownedByMe,owners(me,permissionId,emailAddress,displayName))'};	
-	getAppDataFileList(obj);
+var CLIENT_ID = '14788876155-26tavl9ut4bmlncej9ce21a3tstrj1ca.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyBFvl3ULZmidAM02q-8R_o5bUjWoSne37g';
+
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4","https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest","https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest","https://www.googleapis.com/discovery/v1/apis/people/v1/rest","https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive https://mail.google.com/ https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/contacts https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive.appfolder";
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+function handleClientLoad() {
+	gapi.load('client:auth2', initClient);
 }
 
-function getAppDataFileList(obj) {
-	gapi.client.drive.files.list(obj).then(function(response) {
-		response.result.files.forEach(function(file) {
-			console.log("File found: " + file.id);
-		})
-	},
-	function(err) { 
-		console.error("Execute error",err);
+function initClient() {
+	gapi.client.init({
+		apiKey: API_KEY,
+		clientId: CLIENT_ID,
+		discoveryDocs: DISCOVERY_DOCS,
+		scope: SCOPES
+	}).then(function () {
+		// Listen for sign-in state changes.
+		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+		// Handle the initial sign-in state.
+		updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+		authorizeButton.onclick = handleAuthClick;	//alter buttons as needed - cosmetic
+		signoutButton.onclick = handleSignoutClick;
+	}, function(error) {
+		appendPre(JSON.stringify(error, null, 2));
 	});
 }
 
-function updateUser(key,value,object) {
-	window[object][key]=value;
-	console.log(window[object]);
+function updateSigninStatus(isSignedIn) {
+	if (isSignedIn) {
+		authorizeButton.style.display = 'none';		//handles the appearance of sign in/out buttons
+		signoutButton.style.display = 'block';
+		//do this on login....
+		console.log("Loaded to normal point.");
+		newFunction();
+	} else {
+		authorizeButton.style.display = 'block';
+		signoutButton.style.display = 'none';
+	}
+}
+
+function handleAuthClick(event) {
+	gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignoutClick(event) {
+	gapi.auth2.getAuthInstance().signOut();
 }
