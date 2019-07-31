53b67574cb64046536f63a7d3bee27c6cd722608
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-07-31 19:05:30 -0400

    Update accountSetup.js

--- accountSetup.js
+++ accountSetup.js
@@ -1,5 +1,4 @@
 objects=[
-	{q: "name='App Data'",fields:'files(id,trashed,parents,ownedByMe,owners(me,permissionId,emailAddress,displayName))'},
 	{spaces:'appDataFolder',fields:'files(id,trashed,parents,ownedByMe,owners(me,permissionId,emailAddress,displayName))'}
 ];
 
@@ -13,19 +12,7 @@ function newAccount(email) {
 	window['user']={};
 	updateUser('email',email,'user');
 	console.log(user);
-	getDriveID(objects[0]);
-}
-
-function getDriveID(obj) {
-	gapi.client.drive.files.list(obj).then(function(response) {
-		console.log(response);
-		updateUser('driveKey',response.result.parents[0],'user');
-		getAppDataFileList(objects[1]);
-	},
-	function(err) { 
-		console.error("Execute error",err);
-	})
-	
+	getAppDataFileList(objects[0]);
 }
 
 function getAppDataFileList(obj) {
@@ -57,13 +44,13 @@ function createNewAccount() {
 		gapi.client.sheets.spreadsheets.create(obj).then(function(response) {
 			//update user
 			updateUser('emailSheetKey',response.result.spreadsheetId,'user');
-			//move email sheet
-			obj={addParents:[user.usersFolderKey],removeParents:['drive'],fileId:response.result.spreadsheetId,fields:'*'};
-			gapi.client.drive.files.update(obj).then(function(response) {
+			//update sheet
+			obj={spreadsheetId:user.emailSheetKey,range:'A1',majorDimension:'ROWS',values:[[JSON.stringify(user)]],valueInputOption: 'RAW',fields:'*'};
+			gapi.client.sheets.spreadsheets.values.update(obj).then(function(response) {
 				console.log(response);
-				//update sheet
-				obj={spreadsheetId:user.emailSheetKey,range:'A1',majorDimension:'ROWS',values:[[JSON.stringify(user)]],valueInputOption: 'RAW',fields:'*'};
-				gapi.client.sheets.spreadsheets.values.update(obj).then(function(response) {
+				//move email sheet
+				obj={addParents:[user.usersFolderKey],removeParents:['drive'],fileId:response.result.spreadsheetId,fields:'*'};
+				gapi.client.drive.files.update(obj).then(function(response) {
 					console.log(response);
 					//creation complete?
 					sendEventToAnalytics('user','new',user.email);
