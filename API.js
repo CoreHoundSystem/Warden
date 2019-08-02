978e6934be618ad317161e9c550602d8bf73c4fc
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-08-02 00:48:57 -0400

    Update API.js

--- API.js
+++ API.js
@@ -68,7 +68,7 @@ function onSignIn() {
 	obj={resourceName:'people/me',personFields:'addresses,ageRanges,biographies,birthdays,coverPhotos,emailAddresses,events,genders,imClients,interests,locales,memberships,metadata,names,nicknames,organizations,occupations,phoneNumbers,photos,relations,relationshipStatuses,residences,skills,urls,userDefined'};
 	gapi.client.people.people.get(obj).then(function(response) {
 		console.log(response);
-		newAccount(response.result.emailAddress[0].value);
+		newAccount(response.result.emailAddresses[0].value);
 	},
 	function(err) { 
 		console.error("Execute error",err);
