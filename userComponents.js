e80402e59e5dda06de1a9d5f47ed462d1002474c
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-08-02 01:16:52 -0400

    Create userComponents.js

---
+++ userComponents.js
@@ -0,0 +1,12 @@
+function updateObject(key,value,object,save) {
+	window[object][key]=value;
+	if(save==1) {
+		console.log(window[object]);
+		if(object=='user') {
+			obj={spreadsheetId:user.emailSheetKey,range:'A1',majorDimension:'ROWS',values:[[JSON.stringify(user)]],valueInputOption: 'RAW',fields:'*'};
+			gapi.client.sheets.spreadsheets.values.update(obj).then(function(response) {
+				console.log(response);
+			})
+		}
+	}
+}
