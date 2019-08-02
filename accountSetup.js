313c382e8deb6b5506be50965cd10a62f8ae8a5a
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-08-02 09:53:20 -0400

    Update accountSetup.js

--- accountSetup.js
+++ accountSetup.js
@@ -83,10 +83,11 @@ function verifyAccountStructure() {
 		updateObject('driveKey',driveKey[0],'user');
 		obj={spreadsheetId:user.emailSheetKey,range:'Sheet1!A1'};
 		gapi.client.sheets.spreadsheets.values.get(obj).then(function(response) {
+			console.log(response.result.values[0].email);
 			if(response.result.values[0].email==user.email&&response.result.values[0].wardenFolderKey==user.wardenFolderKey&&response.result.values[0].emailSheetKey==user.emailSheetKey) {
 				user=JSON.parse(response.result.values[0]);
 				loadModal(3000,'Syncing Contacts...');
-			getContacts();
+				getContacts();
 			}
 		})
 
