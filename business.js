4747f3670c95b528b2aa26087a972ad605d9c3e7
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-08-05 17:25:49 -0400

    Update business.js

--- business.js
+++ business.js
@@ -1,3 +1,8 @@
 function createBusiness() {
 	console.log("createBusiness");
+	//updateObject('activeBusiness','creating',user);
+	window['activeBusiness']={status:'create'};
+	console.log(activeBusiness);
+	$('.navCategory').find('span:contains("Business")').parents('.navCategory').click();
+	$('.navCategory').find('span:contains("Business")').parents('.categoryName').next().find('li:contains("Overview")')
 }
