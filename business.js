6016328ab0abb1dd7efab14a27a0d3c0bfc2f2fb
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-08-05 19:12:44 -0400

    Update business.js

--- business.js
+++ business.js
@@ -4,12 +4,21 @@ function createBusiness() {
 	window['activeBusiness']={status:'create'};
 	console.log(activeBusiness);
 	$('#businessModal').remove();
-	if($('.navCategory').find('span:contains("Business")').parents('.navCategory').hasClass('.collapsed')) {
+	if($('.navCategory').find('span:contains("Business")').parents('.navCategory').hasClass('collapsed')) {
 		$('.navCategory').find('span:contains("Business")').parents('.navCategory').click();
 	}
-	if($('.navCategory').find('span:contains("Business")').parents('.categoryName').next().find('li:contains("Overview")').hasClass('.active')) {
+	if($('.navCategory').find('span:contains("Business")').parents('.categoryName').next().find('li:contains("Overview")').hasClass('active')) {
 		
 	} else {
 		$('.navCategory').find('span:contains("Business")').parents('.categoryName').next().find('li:contains("Overview")').click();
 	}
+	/*
+	$('.navCategory span').each(function() {
+		if($(this).text()=="Business") {
+			if($(this).hasClass('collapsed') {
+				$(this).click();
+			}
+		}
+	})
+	*/
 }
