63fb77c31e75ce76d4848fe2c3c8e55185ab2710
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-08-05 20:15:22 -0400

    Update nav.js

--- nav.js
+++ nav.js
@@ -126,9 +126,18 @@ function loadMainContainer(p,c,n) {
 function dataCell(source,tag,type,placeholder) {
 	data='';
 	if(tag=='input') {
-		data='<input type="' + type + '" class="descrete" placeholder="' + placeholder + '" name="' + source + '">';
+		data='<input type="' + type + '" class="descrete" placeholder="' + placeholder + '" name="' + source + '" ' + value(source) + '>';
 	}
 	if(tag=='textarea') {
-		data='<textarea rows=' + type + ' class="descrete" placeholder="' + placeholder + '" name="' + source + '">';
+		data='<textarea rows=' + type + ' class="descrete" placeholder="' + placeholder + '" name="' + source + '" ' + value(source) + '>';
 	}
+	return data
+}
+
+function value(x) {
+	value='';
+	if(typeof x !== 'undefined') {
+		value='value="' + x + '"';
+	}
+	return value
 }
